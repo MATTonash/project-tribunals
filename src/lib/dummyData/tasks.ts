14f7e3a6f78bfe643717bccc182cf85b1422ff86
karleenstile <karleen.han@stileeducation.com>
2023-11-19 17:07:20 +1100

    Extract hard-coded 'tasks' JSON

--- src/lib/dummyData/tasks.ts
+++ src/lib/dummyData/tasks.ts
@@ -2,9 +2,15 @@ import Task from "../Task";
 
 export const tasks: Task[] = [
   {
-    name: "Task 1 Name",
-    description: "Task 1 Description",
+    name: "Judge's Name",
+    description: "The name of the judge/s",
+    entries: [{ type: "shortText", data: "" }],
     key: 1,
-    entries: [],
   },
+  // { name: "Defendant's Name", entryType: "label", entryCount: 0, key: 2 },
+  // { name: "Claimant's Name", entryType: "label", entryCount: 0, key: 3 },
+  // { name: "Reference Number", entryType: "label", entryCount: 1, key: 4 },
+  // { name: "Accusation", entryType: "text", entryCount: 2, key: 5 },
+  // { name: "Task Name", entryType: "multi", entryCount: 3, key: 6 },
+  // { name: "Payout", entryType: "number", entryCount: 1, key: 7 },
 ];
--- src/pages/Annotator.tsx
+++ src/pages/Annotator.tsx
@@ -10,16 +10,6 @@ const Annotator = () => {
   // const { key } = useParams();
   const tasks: Task[] = [];
 
-  // const tasks: Task[] = [
-  //   { name: "Judge's Name", entryType: "label", entryCount: 1, key: "1" },
-  //   { name: "Defendant's Name", entryType: "label", entryCount: 0, key: "1" },
-  //   { name: "Claimant's Name", entryType: "label", entryCount: 0, key: "1" },
-  //   { name: "Reference Number", entryType: "label", entryCount: 1, key: "1" },
-  //   { name: "Accusation", entryType: "text", entryCount: 2, key: "1" },
-  //   { name: "Task Name", entryType: "multi", entryCount: 3, key: "1" },
-  //   { name: "Payout", entryType: "number", entryCount: 1, key: "1" },
-  // ];
-
   return (
     <Flex height="calc(100vh - 64px)">
       <Flex flexDirection="column" width="30vw" maxWidth="500px">
