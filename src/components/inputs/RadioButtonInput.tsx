fa7e15191d4f55f06df8db57d641f616410e0d94
applewendy12 <wendy.huangxi87@outlook.com>
2023-11-29 21:22:26 +1100

    radiobuttoninput?

--- src/components/inputs/RadioButtonInput.tsx
+++ src/components/inputs/RadioButtonInput.tsx
@@ -1,21 +1,27 @@
-import { Radio, RadioGroup } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Radio, RadioGroup } from "@chakra-ui/react";
+import { Stack, HStack, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 
 interface RadioButtonInputProps {
-    choices: string[];
+  choices: string[];
 }
 
-function RadioButtonInput({choices} : RadioButtonInputProps) {
+function RadioButtonInput(RadioButtonInputProps) {
+  const choices = RadioButtonInputProps.choices;
 
+  const [value, setValue] = useState("1");
 
-    const [value, setValue] = useState('1')
-    return (
-      <RadioGroup onChange={setValue} value={value}>
-        <Stack direction='row'>
-          <Radio value='1'>First</Radio>
-          <Radio value='2'>Second</Radio>
-          <Radio value='3'>Third</Radio>
-        </Stack>
-      </RadioGroup>
-    )
-  }
+  return (
+    <RadioGroup
+      onChange={setValue}
+      value={value}>
+      <Stack direction="row">
+        {choices.map((choice) => (
+          <Radio value={choice.index}>{choice.option}</Radio>
+        ))}
+      </Stack>
+    </RadioGroup>
+  );
+}
+
+export default RadioButtonInput;
