00c20a0b7ad163e470b56f242736abcc840579e1
DanielArnould <darn0005@student.monash.edu>
2023-12-06 11:58:39 +1100

    Added start tasks toast button
    When selecting tasks, a "start tasks" button appears in the bottom left. This is intended to begin a task sequence.

--- src/components/ResizableSidebar.tsx
+++ src/components/ResizableSidebar.tsx
@@ -1,9 +1,17 @@
-import { Divider, Flex } from "@chakra-ui/react";
-import { useEffect, useRef, useState } from "react";
-import Task from "../lib/Task";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Spacer,
+  ToastId,
+  useToast,
+} from "@chakra-ui/react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import SidebarHeader from "./SidebarHeader";
 import TaskItem from "./TaskItem";
 import { TaskInputs } from "./input/TaskInputs";
+import { Task } from "../lib/Task";
 
 interface ResizableSidebarProps {
   tasks: Array<Task>;
@@ -13,9 +21,36 @@ const ResizableSidebar = ({ tasks }: ResizableSidebarProps) => {
   const [isResizing, setIsResizing] = useState(false);
   const [sidebarWidth, setSidebarWidth] = useState(0);
 
-  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+  const [currentTask, setCurrentTask] = useState<Task | null>(null);
+  const [selectedTasks, setSelectedTasks] = useState<Array<Task>>([]);
+  const [listedTasks, setListedTasks] = useState<Array<Task>>(tasks);
 
   const dividerRef = useRef<HTMLHRElement | null>(null);
+  const toast = useToast();
+  const toastIdRef = useRef<ToastId | null>(null);
+
+  useEffect(() => {
+    if (selectedTasks.length > 0 && !toastIdRef.current) {
+      toastIdRef.current = toast({
+        position: "bottom-left",
+        duration: null,
+
+        render: () => <Button>Start tasks</Button>,
+      });
+    } else if (selectedTasks.length === 0 && toastIdRef.current) {
+      toast.close(toastIdRef.current);
+      toastIdRef.current = null;
+    }
+  }, [selectedTasks]);
+
+  useEffect(() => {
+    return () => {
+      if (toastIdRef.current) {
+        toast.close(toastIdRef.current);
+        toastIdRef.current = null;
+      }
+    };
+  }, []);
 
   const handleMouseDown = () => {
     setIsResizing(true);
@@ -28,6 +63,17 @@ const ResizableSidebar = ({ tasks }: ResizableSidebarProps) => {
   const handleMouseMove = (event: MouseEvent) => {
     if (isResizing) {
       setSidebarWidth(event.clientX);
+    }
+  };
+
+  const handleCheckboxChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    task: Task
+  ) => {
+    if (event.target.checked) {
+      setSelectedTasks(selectedTasks.concat(task));
+    } else {
+      setSelectedTasks(selectedTasks.filter((t) => t !== task));
     }
   };
 
@@ -57,22 +103,33 @@ const ResizableSidebar = ({ tasks }: ResizableSidebarProps) => {
     >
       <Flex minWidth={"calc(100% - 10px)"} flexDirection="column">
         <SidebarHeader
-          task={selectedTask}
+          task={currentTask}
           addTask={() => {}}
-          deleteTask={() => {}}
-          goBack={() => setSelectedTask(null)}
+          deleteTask={() => {
+            setListedTasks(
+              listedTasks.filter((task) => !selectedTasks.includes(task))
+            );
+            setSelectedTasks([]);
+          }}
+          goBack={() => setCurrentTask(null)}
         />
-        {selectedTask ? (
-          <Flex flexDirection="column" gap={4} padding={2}>
-            <TaskInputs task={selectedTask} />
+        {currentTask ? (
+          <Flex flexDirection="column" gap={4} padding={2} height={"100%"}>
+            <TaskInputs task={currentTask} />
+            <Spacer />
+            <Button>Save</Button>
           </Flex>
         ) : (
-          tasks.map((task, index) => (
+          listedTasks.map((task, index) => (
             <TaskItem
               task={task}
-              key={task.key}
+              key={task.id}
               isAlternate={index % 2 === 1}
-              onClick={() => setSelectedTask(task)}
+              onClick={() => {
+                setSelectedTasks([]);
+                setCurrentTask(task);
+              }}
+              onCheckboxChange={(event) => handleCheckboxChange(event, task)}
             />
           ))
         )}
