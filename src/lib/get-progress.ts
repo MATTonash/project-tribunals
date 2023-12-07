3bb857ec5369b07c188e7a912add8ae06d726f97
DanielArnould <darn0005@student.monash.edu>
2023-12-08 00:04:51 +1100

    Added a task progress calculator
    Simply returns a fraction of how many tasks a user has completed in  a document out of the total amount of tasks.

---
+++ src/lib/get-progress.ts
@@ -0,0 +1,22 @@
+import { documentsDb } from "./dummy-data/documentsDb";
+
+export const getProgress = (
+  documentId: string
+): { completedTasks: number; totalTasks: number } => {
+  const document = documentsDb[documentId];
+
+  if (!document || !document.tasks) {
+    return { completedTasks: 0, totalTasks: 0 };
+  }
+
+  const tasks = Object.values(document.tasks);
+  const completedTasks = tasks.reduce(
+    (count, task) => count + (task.status === "complete" ? 1 : 0),
+    0
+  );
+
+  return {
+    completedTasks,
+    totalTasks: tasks.length,
+  };
+};
