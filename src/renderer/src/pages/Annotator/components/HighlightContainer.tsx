14390a0f20344e24f03b174e31c21b3ba7458cba
DanielArnould <darn0005@student.monash.edu>
2024-04-02 00:08:51 +1100

    merge highlight container and popup

--- src/renderer/src/pages/Annotator/components/HighlightContainer.tsx
+++ src/renderer/src/pages/Annotator/components/HighlightContainer.tsx
@@ -1,42 +1,44 @@
-import { MouseEvent } from "react";
+import { MouseEvent } from 'react'
 import {
   MonitoredHighlightContainer,
   TextHighlight,
   ViewportHighlight,
-  useHighlightContainerContext,
-} from "react-pdf-highlighter-extended";
-import HighlightPopup from "./HighlightPopup";
+  useHighlightContainerContext
+} from 'react-pdf-highlighter-extended'
+import { ScaleFade, Tag } from '@chakra-ui/react'
+import { TaskHighlight } from 'src/common/types'
 
 export interface HighlightContainerProps {
-  onContextMenu?: (
-    event: MouseEvent<HTMLDivElement>,
-    highlight: ViewportHighlight,
-  ) => void;
+  onContextMenu?: (event: MouseEvent<HTMLDivElement>, highlight: ViewportHighlight) => void
 }
 
 const HighlightContainer = ({ onContextMenu }: HighlightContainerProps) => {
-  const { highlight, isScrolledTo } = useHighlightContainerContext();
+  const { highlight, isScrolledTo } = useHighlightContainerContext()
 
   const component = (
     <TextHighlight
       isScrolledTo={isScrolledTo}
       highlight={highlight}
-      onContextMenu={(event) =>
-        onContextMenu && onContextMenu(event, highlight)
-      }
+      onContextMenu={(event) => onContextMenu && onContextMenu(event, highlight)}
     />
-  );
+  )
 
   return (
     <MonitoredHighlightContainer
       highlightTip={{
         position: highlight.position,
-        content: <HighlightPopup />,
+        content: (
+          <ScaleFade in={true} unmountOnExit={true}>
+            <Tag size="lg" variant={'solid'} colorScheme="stormGray">
+              {(highlight as ViewportHighlight<TaskHighlight>).fieldTypeName}
+            </Tag>
+          </ScaleFade>
+        )
       }}
       key={highlight.id}
       children={component}
     />
-  );
-};
+  )
+}
 
-export default HighlightContainer;
+export default HighlightContainer
